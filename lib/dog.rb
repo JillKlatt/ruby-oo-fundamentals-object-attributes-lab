4682e7e9690b7abb64335cab1965d1646e1bf29a
JillKlatt <klatt.jill@gmail.com>
2021-02-09 10:52:31 -0600

    commit tree 0ba10de8629c02fab423fce1511bac1f2dd6cd97

---
+++ lib/dog.rb
@@ -0,0 +1,40 @@
+class Dog
+    # def initialize(name, breed)
+    #     @name = name
+    #     @breed = breed
+    # end
+    def name=(name)
+        @name = name
+    end
+    def name
+        @name
+    end
+    def breed=(breed)
+        @breed = breed
+    end
+    def breed
+        @breed
+    end
+end
+
+# new_dog = Dog.new("Spot", "Mutt")
+
+
+
+# class Dog
+#     def initialize(name, breed)
+#     @name = name
+#     @breed = breed
+#     end
+#     def name=(name)
+#         @name = name
+#     end
+#     def name
+#         @name
+#     end
+# end
+    
+# Puppy =
+# Dog.new("Puppy", "Mutt")
+
+# Dog.new
